e5d7a082baec5f479e29c2e9413c80e9f1d31d40
Weber Yang <weberyanglalala@gmail.com>
2024-10-17 00:53:48 +0800

    fix(auth): add data model validation

--- Dotnet8LineRichMenu/WebApi/Auth/Dtos/LoginUserModel.cs
+++ Dotnet8LineRichMenu/WebApi/Auth/Dtos/LoginUserModel.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Dotnet8LineRichMenu.WebApi.Auth.Dtos;
 
 public class LoginUserModel
 {
+    [EmailAddress]
+    [Required]
     public string Email { get; set; }
+    
+    [MaxLength(50)]
+    [Required]
     public string Password { get; set; }
 }
--- Dotnet8LineRichMenu/WebApi/Auth/Dtos/RegisterUserModel.cs
+++ Dotnet8LineRichMenu/WebApi/Auth/Dtos/RegisterUserModel.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Dotnet8LineRichMenu.WebApi.Auth.Dtos;
 
 public class RegisterUserModel
 {
+    [EmailAddress]
+    [Required]
     public string Email { get; set; }
+    [MaxLength(50)]
+    [Required]
     public string Password { get; set; }
 }
